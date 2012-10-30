0b97900d2a94df2582cd2d5109ae841e48f1cca6
Allen Vailliencourt <chumpmonkey@gmail.com>
2012-10-30 13:34:50 -0400

    Initial commit
    Dumping it to Git for the world to see! Yay!

---
+++ BackgroundWorker_Test/BackgroundWorker_Test/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BackgroundWorker_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.startAsyncButton = new System.Windows.Forms.Button();
+            this.cancelAsyncButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(29, 23);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(59, 13);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.Text = "Progress %";
+            // 
+            // startAsyncButton
+            // 
+            this.startAsyncButton.Location = new System.Drawing.Point(32, 110);
+            this.startAsyncButton.Name = "startAsyncButton";
+            this.startAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.startAsyncButton.TabIndex = 1;
+            this.startAsyncButton.Text = "Start";
+            this.startAsyncButton.UseVisualStyleBackColor = true;
+            this.startAsyncButton.Click += new System.EventHandler(this.startAsyncButton_Click);
+            // 
+            // cancelAsyncButton
+            // 
+            this.cancelAsyncButton.Location = new System.Drawing.Point(202, 110);
+            this.cancelAsyncButton.Name = "cancelAsyncButton";
+            this.cancelAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelAsyncButton.TabIndex = 2;
+            this.cancelAsyncButton.Text = "Cancel";
+            this.cancelAsyncButton.UseVisualStyleBackColor = true;
+            this.cancelAsyncButton.Click += new System.EventHandler(this.cancelAsyncButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(32, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(245, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 162);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelAsyncButton);
+            this.Controls.Add(this.startAsyncButton);
+            this.Controls.Add(this.resultLabel);
+            this.Name = "Form1";
+            this.Text = "Background Worker Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button startAsyncButton;
+        private System.Windows.Forms.Button cancelAsyncButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+A siimple application that demonstrates the Background Worker process in C#. Most of the code and inspiration came from these links -
+
+MSDN - http://msdn.microsoft.com/en-us/library/system.componentmodel.backgroundworker.aspx
+
+Answer to my question on Stackoverflow - http://stackoverflow.com/a/1063583
